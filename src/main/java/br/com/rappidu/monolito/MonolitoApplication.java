16bc62ff3c8d529244308be189ce5d28b23a8c8a
John Gomes <silva.johngomes@gmail.com>
2024-04-15 21:45:47 -0300

    first commit

---
+++ src/main/java/br/com/rappidu/monolito/MonolitoApplication.java
@@ -0,0 +1,13 @@
+package br.com.rappidu.monolito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonolitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonolitoApplication.class, args);
+	}
+
+}
